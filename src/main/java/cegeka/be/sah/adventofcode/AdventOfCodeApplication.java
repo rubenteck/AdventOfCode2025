b833dd044acfa1e8255fca57650c700d81c0ce28
rubente <ruben.teck@cegeka.com>
2024-12-13 12:58:09 +0100

    init

---
+++ src/main/java/cegeka/be/sah/adventofcode/AdventOfCodeApplication.java
@@ -0,0 +1,13 @@
+package cegeka.be.sah.adventofcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventOfCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventOfCodeApplication.class, args);
+    }
+
+}
