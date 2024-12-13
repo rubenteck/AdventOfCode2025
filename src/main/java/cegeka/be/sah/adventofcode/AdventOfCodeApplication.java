a15bc7faaecedc1348252b2874ccafada863fa31
rubente <ruben.teck@cegeka.com>
2024-12-13 13:02:18 +0100

    excalidraw

--- src/main/java/cegeka/be/sah/adventofcode/AdventOfCodeApplication.java
+++ src/main/java/cegeka/be/sah/adventofcode/AdventOfCodeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AdventOfCodeApplication {
 
     public static void main(String[] args) {
+        System.out.println("we win");
         SpringApplication.run(AdventOfCodeApplication.class, args);
     }
 
