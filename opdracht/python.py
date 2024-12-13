fe5a096f5b193fffb69d6970622dd1e3ea06cce5
Aloïs Van de Voorde <alois.vandevoorde@cegeka.com>
2024-12-13 14:02:56 +0100

    python solution  solved

--- opdracht/python.py
+++ opdracht/python.py
@@ -17,7 +17,16 @@ def get_most_popular_item(data):
     popular_items = []
     max_quantity = 0
     for item in data:
+        if item['quantity'] == max_quantity:
+            popular_items.append(item['name'])
+        elif item['quantity'] > max_quantity:
+            max_quantity = item['quantity']
+            popular_items = [item['name']]
+
+    popular_items.sort()
+    return popular_items[0]
+
 
 print(sort_by_type(data))
-
+print(get_most_popular_item(data))
 # {'Toy': 1729, 'Candy': 1785, 'Clothing': 1982}
